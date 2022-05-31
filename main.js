a57bef1a17fd4df047138ca239b1c20ef704ec98
maverick-farhan <mdfarhanofficial@protonmail.com>
2022-05-31 14:34:03 +0530

    Commiting Files

---
+++ main.js
@@ -0,0 +1,66 @@
+"use strict";
+
+// Print any string on console output
+// console.log("Garo Is Beast");
+// console.log("12");
+// Use of Variables
+
+// var let const
+// const numpi = 3.14;
+// console.log(numpi);
+// numpi = 3.15;
+// console.log(numpi);
+// We cannot change const values here;
+
+
+// var myname = "Garo";
+// console.log(myname);
+// myname = "farhan";
+// console.log(myname);
+
+// let firstname = "Garou";
+// console.log(firstname);
+
+// let firstName = "Disco";
+// console.log(firstName);
+
+// Naming VAriables........
+// var 1value = "12";Error
+
+// var good123 = "OK"
+// console.log(good123);
+
+// let _farhan = 12;
+// console.log(_farhan ** .5);
+
+// let far$han = 12;
+// console.log(_farhan ** .5);
+
+// Snake case Vs Camel case
+
+// var name_farhan = "Farhan";/*Snake Case */
+
+// var nameFirst = "First";/* Camel Case*/
+// console.log(nameFirst);
+// console.log(name_farhan);
+
+// Block scope vs function scope
+// let blockScope = "Some Function reasons Let is must to use";
+// blockScope = "Illustrator";
+// console.log(blockScope);
+
+// const pi = "12.2";
+// console.log(pi);
+
+// Area of circle program
+// let radius = 92;
+// const pi = 3.14;
+// let area = 2 * pi * radius;
+// console.log(area);
+
+// string indexing
+
+// let first_String = "OT3";
+// first_String[3];
+// console.log(first_String[first_String.length -1]);
+
---
+++ readme.md
@@ -0,0 +1,14 @@
+From now onwards we will try to make industry level decisions and designs.
+Responsive and More engaging Features and more problems solving aproach towards design.
+Mobile awas of desktop design
+
+Work on more templates and designs for clients and samples design
+Product based and services brands and companies.
+
+Serious designs and advances into tecjnology.
+
+From now onwards work on html and css javascript programming
+
+Desktop and Mobile version apps and sites for various niches from fashion to pharmacy, aeronautics to salons.
+
+Make more appealing designs.
